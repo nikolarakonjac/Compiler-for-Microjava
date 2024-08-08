f2e6bfe912807a75cdb00267b82c3503e396f400
rn200637d <rn200637d@student.etf.bg.ac.rs>
2024-08-08 18:24:41 +0200

    leksicka analiza zavrsena

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/sym.java
@@ -0,0 +1,66 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym {
+	
+	// keywords
+	public static final int PROGRAM = 1;
+    public static final int BREAK = 2;
+    public static final int CLASS = 3;
+    public static final int ELSE = 4;
+    public static final int CONST = 5;
+    public static final int IF = 6;
+    public static final int NEW = 7;
+    public static final int PRINT = 8;
+    public static final int READ = 9;
+    public static final int RETURN = 10;
+    public static final int VOID = 11;
+    public static final int EXTENDS = 12;
+    public static final int CONTINUE = 13;
+    public static final int FOR = 14;
+    public static final int STATIC = 15;
+    public static final int NAMESPACE = 16;
+    public static final int RANGE = 49;
+    
+	
+	// identifiers
+	public static final int IDENT = 17;
+	
+	// constants
+	public static final int NUMBER = 18;
+	public static final int CHAR = 19;
+	public static final int BOOL = 20;
+	
+	// operators
+	public static final int PLUS = 21;			// +
+    public static final int MINUS = 22;			// -
+    public static final int MUL = 23;			// *
+    public static final int DIV = 24;			// /
+    public static final int MOD = 25;			// %
+    public static final int EQUAL = 26;			// ==
+    public static final int NOT_EQUAL = 27;		// !=
+    public static final int GREATER = 28;		// >
+    public static final int GREATER_EQUAL = 29;	// >= 
+    public static final int LESS = 30;			// <
+    public static final int LESS_EQUAL = 31;	// <=
+    public static final int AND = 32;			// &&
+    public static final int OR = 33;			// ||
+    public static final int ASSIGN = 34;		// =
+    public static final int INC = 35;			// ++
+    public static final int DEC = 36;			// --
+    public static final int SEMICOLON = 37;		// ;
+    public static final int COLON = 38;			// :
+    public static final int COMMA = 39;			// ,
+    public static final int DOT = 40;			// .
+    public static final int LPAREN = 41;		// (
+    public static final int RPAREN = 42;		// ) 
+    public static final int LBRACKET = 43;		// [
+    public static final int RBRACKET = 44;		// ]
+    public static final int LBRACE = 45;		// {
+    public static final int RBRACE = 46;		// }
+    public static final int ARROW = 47;			// =>
+	
+	// end of file
+	public static final int EOF = 48;
+	
+		
+}
