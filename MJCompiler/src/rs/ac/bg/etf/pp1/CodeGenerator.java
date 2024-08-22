c2b5214ba4c9cbcf691598c9b2c651b29c4b6dea
rn200637d <rn200637d@student.etf.bg.ac.rs>
2024-08-22 16:10:22 +0200

    4 faza; alokacija niza, pristup elementu niza, dodela vrednosti elementu niza i minus operacija

--- MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -7,6 +7,7 @@ import rs.ac.bg.etf.pp1.CounterVisitor.VarCounter;
 import rs.ac.bg.etf.pp1.ast.*;
 import rs.etf.pp1.mj.runtime.Code;
 import rs.etf.pp1.symboltable.concepts.Obj;
+import rs.etf.pp1.symboltable.concepts.Struct;
 
 public class CodeGenerator extends VisitorAdaptor {
 	
@@ -67,10 +68,12 @@ public class CodeGenerator extends VisitorAdaptor {
 	// ---------------------------------------------- PRINT -------------------------------------------
 	
 	public void visit(StatementPrintExpr printStm) {
+		// Statement ::= PRINT LPAREN Expr RPAREN SEMICOLON
+		
 		// ... , val, width
 		//val treba vec da se izracuna u expr i stavi na stek, tako da mi samo stavljamo width
+		
 		if(printStm.getExpr().struct == MyTab.intType || printStm.getExpr().struct == MyTab.boolType) {
-			//int ili char
 			Code.loadConst(5);	//width
 			Code.put(Code.print);
 		}
@@ -85,6 +88,7 @@ public class CodeGenerator extends VisitorAdaptor {
 	
 	public void visit(FactorNumber num) {
 		Obj numConst = MyTab.insert(Obj.Con, "$", num.struct);
+		
 		numConst.setLevel(0);
 		numConst.setAdr(num.getN1());	//vrednost konstante cemo pokupiti iz leksickog analizatora
 		// konstante se javljaju u okviru izraza i treba je staviti na exprStack
@@ -92,12 +96,69 @@ public class CodeGenerator extends VisitorAdaptor {
 		Code.load(numConst);	// na exprStack se stavi objektni cvor
 	}
 	
+	public void visit(FactorChar charConst) {
+		Obj newCharConst = MyTab.insert(Obj.Con, "$", charConst.struct);
+		
+		newCharConst.setLevel(0);
+		newCharConst.setAdr(charConst.getC1());  	// vrednost konstante je samo ime konstane
+		
+		Code.load(newCharConst);
+	}
 	
+	public void visit(FactorBool boolConst) {
+		Obj newBoolConst = MyTab.insert(Obj.Con, "$", boolConst.struct);
+		
+		newBoolConst.setLevel(0);
+		
+		if(boolConst.getB1()) {
+			newBoolConst.setAdr(1);
+		}
+		else {
+			newBoolConst.setAdr(0);
+		}
+		
+		Code.load(newBoolConst);
+	}
+	
+	public void visit(NewFactorArray arrayAlocation) {
+		// NewFactor ::= NEW Type:arrType LBRACKET Expr:arraySize RBRACKET
+		
+		//expr:arraySize ce vec biti na steku
+		
+		Code.put(Code.newarray);
+		if(arrayAlocation.getType().struct == MyTab.intType || arrayAlocation.getType().struct == MyTab.boolType) {
+			// newarray 1 -> niz reci
+			Code.put(1);
+		}
+		else {
+			//newarray 0 -> niz bajtova  za char type
+			Code.put(0);
+		}
+		   
+	}
+	
+	// ---------------------------------------------- Term -------------------------------------------
+	
+	public void visit(TermMul mul) {
+		// Term ::= Term Mulop Factor
+		
+		// elementi koji ucestvuju u izrazu su vec na steku
+		Code.put(Code.mul);
+	}
+
 	// ---------------------------------------------- Expr -------------------------------------------
 	
 	public void visit(ExprAddTerm add) {
+		// Expr ::= Expr:expr Addop Term:term
+		
 		// elementi koji ucestvuju u izrazu su vec na steku
 		Code.put(Code.add);
+	}
+	
+	public void visit(ExprMinusTerm minus) {
+		// Expr ::= MINUS Term:t
+		
+		Code.put(Code.neg);
 	}
 	
 	
@@ -106,8 +167,34 @@ public class CodeGenerator extends VisitorAdaptor {
 	public void visit(DesignatorStatementAssign assignment) {
 		// DesignatorStatement ::=  Designator:destination Assignop Expr:value SEMICOLON
 		
-		// dohvatamo vrednost exprStacka(jer smatramo da je expr vec postavio na stek vrednost) i sacuvamo je u destination
-		Code.store(assignment.getDesignator().obj);	//odgovaraju instrukcija ce se generisati na osnovu prirode cvora
+		SyntaxNode designatorParent = assignment.getParent();
+		SyntaxNode exprParent = assignment.getExpr().getParent();
+		
+		
+		
+		if(designatorParent.getClass().equals(DesignatorArray.class)){
+			// dodela vrednosti elementu niza
+			
+			Struct arrayType = assignment.getDesignator().obj.getType().getElemType();
+			
+			if(arrayType == MyTab.intType || arrayType == MyTab.boolType ) {
+				Code.put(Code.astore);
+			}
+			else if(arrayType == MyTab.charType) {
+				Code.put(Code.bastore);
+			}
+			else {
+				report_info("pri dodeli vrednosti designator je bio matrica", assignment);
+			}
+			
+		}
+		else {
+			// dohvatamo vrednost exprStacka(jer smatramo da je expr vec postavio na stek vrednost) i sacuvamo je u destination
+			Code.store(assignment.getDesignator().obj);	//odgovaraju instrukcija ce se generisati na osnovu prirode cvora
+		}
+		
+		
+		
 	}
 	
 	
@@ -118,15 +205,62 @@ public class CodeGenerator extends VisitorAdaptor {
 		
 		SyntaxNode parent = des.getParent();	
 		
-		if(!parent.getClass().equals(DesignatorStatementAssign.class)) {
+		// za ove slucajeve roditeljskih klasa ne treba da se stavlja na stek designator
+		// jer ili nema potrebe ili se to radi u toj roditeljskoj klasi kao npr za inc 
+		if(!(
+				parent.getClass().equals(DesignatorStatementAssign.class) ||
+				parent.getClass().equals(DesignatorStatementInc.class) ||
+				parent.getClass().equals(DesignatorStatementDec.class)
+				)) {
 			Code.load(des.obj);	
+		}
+
+	}
+	
+	public void visit(DesignatorArray array) {
+		// Designator ::= Designator:designatorName LBRACKET Expr:expr RBRACKET
+		
+		
+		SyntaxNode parent = array.getParent();
+		
+		// u DesignatorIdent (jer se on prvi posecuje)  stavimo na stek designator -> niz
+		// expr ce staviti const koja je arraySize na stek
+		
+		
+		if(parent.getClass().equals(DesignatorFactorDesignator.class)) {
+			// ovde se dolazi za slucaj print(niz[5])
+			Code.put(Code.aload);	// dohvati vrednost elementa niza i stavi na stek, spremno je za print koje ocekuje vrednost za ispis
 		}
 		
 		
-	
 	}
 	
 	
+	// ---------------------------------------------- Designator INC  -------------------------------------------
+	
+	public void visit(DesignatorStatementInc designatorInc) {
+		// DesignatorStatement ::= Designator:designator INC SEMICOLON
+		
+		Obj designator = designatorInc.getDesignator().obj;
+		
+		Code.load(designator);	// x 
+		Code.put(Code.const_1);	// x 1 
+		Code.put(Code.add);		// (x+1)
+		Code.store(designator); // x = x + 1
+	}
+	
+	// ---------------------------------------------- Designator DEC  -------------------------------------------
+	
+	public void visit(DesignatorStatementDec designatorDec) {
+		// DesignatorStatement ::= Designator:designator DEC SEMICOLON
+		
+		Obj designator = designatorDec.getDesignator().obj;
+		
+		Code.load(designator);
+		Code.put(Code.const_1);
+		Code.put(Code.sub);		
+		Code.store(designator);
+	}
 	
 }
 
